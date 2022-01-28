795d61c7d161eac67ac130a1d0bfac06cd9c51a7
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-01-29 03:29:37 +0530

    Profile code Update

--- pay/views.py
+++ pay/views.py
@@ -1,3 +1,4 @@
+from urllib.request import Request
 from django.shortcuts import render
 from django.views import View
 
@@ -14,3 +15,12 @@ class Home(View):
     def post(self, request):
         email = request.POST.get('email')
         return render(request, 'pay/app.html')
+    
+def gitRepo():
+    username = input("Enter the github username:")
+    request = Request.get('https://api.github.com/users/'+username+'/repos')
+    json = request.json()
+    for i in range(0,len(json)):
+        print("Project Number:",i+1)
+        print("Project Name:",json[i]['name'])
+        print("Project URL:",json[i]['svn_url'],"\n")
