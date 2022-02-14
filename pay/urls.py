7315a1d2ec267e317b5578bcf5fb6380467e1456
rock12231 <37548802+rock12231@users.noreply.github.com>
2022-02-14 14:38:04 +0530

    login part done

--- pay/urls.py
+++ pay/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from pay.views import Home
-
+from pay import views
 urlpatterns = [
        path('', Home.as_view(), name='home'),
+       path('profile/', views.Login, name='login'),
+       path('', views.Logout, name='logout'),   
 ]
--- pay/views.py
+++ pay/views.py
@@ -1,20 +1,39 @@
 from urllib.request import Request
-from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
-
-# def index(request):
-#     return render(request, "pay/app.html")
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login, logout
 
 
 class Home(View):
     def get(self, request):
         return render(request, 'pay/app.html')
 
-    def post(self, request):
-        email = request.POST.get('email')
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # print(username, password,"username........, password.......")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print("Login Successful")
+            return render(request, 'pay/app.html',{'user':user})
+        else:
+            print("Login Failed")
         return render(request, 'pay/app.html')
+
+def Logout(request):
+    logout(request)
+    return redirect('home')
+    
+    
+    
     
 def gitRepo():
     username = input("Enter the github username:")
@@ -24,3 +43,13 @@ def gitRepo():
         print("Project Number:",i+1)
         print("Project Name:",json[i]['name'])
         print("Project URL:",json[i]['svn_url'],"\n")
+        
+        
+
+@login_required
+def secret_page(request):
+    return render(request, 'secret_page.html')
+
+
+class SecretPage(LoginRequiredMixin, TemplateView):
+    template_name = 'secret_page.html'
